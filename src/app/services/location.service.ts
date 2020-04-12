11f96e81e6fb1d0dfc2978495c3d8b12aa0bf65c
andres002345 <aspa101@hotmail.com>
2020-04-12 10:43:58 -0300

    Issue 2 filtros

--- src/app/services/location.service.ts
+++ src/app/services/location.service.ts
@@ -1,13 +1,30 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http'; 
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import { Province } from '../models/province-model';
+import { Department } from '../models/department-model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LocationService {
 
-  constructor(private http: HttpClient) { }
-  getJson(url:string){
-    return this.http.get(url);
+  constructor(private http:HttpClient) { }
+
+  readonly APIUrl = "https://abc-back.herokuapp.com/api/locations/provinces";
+  readonly APIUrl2 = "https://abc-back.herokuapp.com/api/locations/departments?provinceId=";
+  APIUrl3 = "";
+
+  getLocationList(Province): Observable<any[]>{
+    let response1 = this.http.get<Province[]>(this.APIUrl);
+    
+    return response1;
   }
+
+  getDepartmentList(Department, provinceId): Observable<any[]>{
+    this.APIUrl3 = this.APIUrl2 + provinceId;
+    let response2 = this.http.get<Department[]>(this.APIUrl3);
+    return response2;
+  }
+  
 }
