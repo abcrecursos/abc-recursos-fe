dfd544011000408e9b564bf44d8e50c3baa0da45
Micaela Iribarren <micaela_iribarren@hotmail.com>
2020-04-26 17:54:53 -0300

    Add table places to donate

--- src/app/pages/donate/donate.component.ts
+++ src/app/pages/donate/donate.component.ts
@@ -1,5 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { SuppliesService } from '../../services/suppplies.service';
+import {MatTableDataSource} from '@angular/material/table';
+import {MatSort} from '@angular/material/sort';
+
+export interface PeriodicElement {
+  position: number;
+  nombre: string;
+  direccion: string;
+  distancia: string;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  {position: 1, nombre: 'Hospital 1', direccion: 'Mitre 747', distancia: '300 mts'},
+  {position: 2, nombre: 'Hospital 2', direccion:  'San Luis 987', distancia:'1200 mts'},
+];
 
 @Component({
   selector: 'app-donate',
@@ -7,6 +21,21 @@ import { SuppliesService } from '../../services/suppplies.service';
   styleUrls: ['./donate.component.css']
 })
 export class DonateComponent implements OnInit {
+
+  displayedColumns: string[] = ['nombre', 'direccion', 'distancia'];
+  dataSource = new MatTableDataSource(ELEMENT_DATA);
+
+  @ViewChild(MatSort) sort: MatSort;
+
+  ngAfterViewInit() {
+    this.dataSource.sort = this.sort;
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+
   supplies: any;
   showMe: boolean = false
   order: string;
