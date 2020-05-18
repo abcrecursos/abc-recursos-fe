680b502438f1a1db4e534a0616c6f5676f468c34
andres002345 <aspa101@hotmail.com>
2020-05-18 01:34:34 -0300

    pedidos de insumos

--- src/app/services/location.service.ts
+++ src/app/services/location.service.ts
@@ -1,38 +1,29 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import { HttpRequesterService } from './http-requester.service';
 import {Observable} from 'rxjs';
-import { Province } from '../models/province-model';
-import { Locality } from '../models/locality-model';
-import { Effector } from '../models/effector-model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LocationService {
+  private static getProvinces: string = "locations/provinces";
+  private static getLocality: string = "health-centers/localitieslist?provinceId=";
+  private static getEffector: string = "health-centers/localities?localityId=";
 
-  constructor(private http:HttpClient) { }
-
-  readonly APIUrl = "https://abc-back.herokuapp.com/api/locations/provinces";
-  readonly APIUrl2 = "https://abc-back.herokuapp.com/api/health-centers/localitieslist?provinceId=";
-  APIUrl3 = "";
-  readonly APIUrl4 = "https://abc-back.herokuapp.com/api/health-centers/localities?localityId=";
-  APIUrl5 = "";
+  constructor(private httpRequester: HttpRequesterService) { }
 
   getLocationList(Province): Observable<any[]>{
-    let response1 = this.http.get<Province[]>(this.APIUrl); 
-    return response1;
+    return this.httpRequester.doGet(LocationService.getProvinces);
   }
 
   getLocalityList(Locality, provinceId): Observable<any[]>{
-    this.APIUrl3 = this.APIUrl2 + provinceId;
-    let response2 = this.http.get<Locality[]>(this.APIUrl3);
-    return response2;
+    const auxiliar = LocationService.getLocality + provinceId;
+    return this.httpRequester.doGet(auxiliar);
   }
 
   getEffectorList(Effector, localityId): Observable<any[]>{
-    this.APIUrl5 = this.APIUrl4 + localityId;
-    let response3 = this.http.get<Locality[]>(this.APIUrl5);
-    return response3;
+    const auxiliar2 = LocationService.getEffector + localityId;
+    return this.httpRequester.doGet(auxiliar2);
   }
   
 }
